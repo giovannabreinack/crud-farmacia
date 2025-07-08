f72733d4ea385ae6de59c2df3aac6af966ee8175
Gih <giovannabreinackcolombara@gmail.com>
2025-07-08 10:39:43 -0300

    add pasta e classes da categoria

---
+++ src/categoria/entities/categoria.entity.ts
@@ -0,0 +1,16 @@
+import { IsNotEmpty } from "class-validator";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({ name: "tb_categorias" })
+export class Categoria {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @IsNotEmpty()
+    @Column({ length: 150, nullable: false })
+    nome: string
+
+    @Column({ length: 255, nullable: true })
+    descricao: string
+}
